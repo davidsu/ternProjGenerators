eda6c53bb660c23351fefb6b282e3a2c1159dbdd
David <davidsu@wix.com>
2017-04-03 19:54:25 +0300

    yesterdays push was fucked up, the tern proj did not work at all. this is a good version

--- tern_proj_generator_santa.js
+++ tern_proj_generator_santa.js
@@ -7,14 +7,14 @@ function run(baseDir) {
     spawn.on('close', function() {
         var resArr = result.split('\n')
         result = {
-            "ecmaVersion": 5,
+            // "ecmaVersion": 5,
             "libs": [
                 "browser"
             ],
             "plugins": {
-                "node": {},
-                "modules": {},
-                "es_modules": {},
+                // "node": {},
+                // "modules": {},
+                // "es_modules": {},
                 "requirejs": {
                     "baseUrl": "./",
                     paths: {},
@@ -25,28 +25,31 @@ function run(baseDir) {
         var paths = result.plugins.requirejs.paths;
         var map = result.plugins.requirejs.map;
         resArr.forEach(curr => {
-            var key1 = curr.replace(/.*packages\/(.*)src\/main\/([^.]*)\.js$/, '$1$2')
-            var key2 = curr.replace(/.*\/([^/.]*)\.js$/, '$1');
-            if (key1 && key1 !== curr) {
-                paths[key1] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-            }
-            if (key2 && key2 !== curr) {
-                var mapkey = curr.replace(/.*(packages\/.*?)\/src\/main\/.*/, '$1')
-                map[mapkey] = map[mapkey] || {};
-                map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-                mapkey = curr.replace(/.*packages\/(.*?)\/src\/main\/.*/, '$1')
-                map[mapkey] = map[mapkey] || {};
-                map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-                mapkey = curr.replace(/.*packages\/.*?\/src\/main\/([^/]).*/, '$1')
-                map[mapkey] = map[mapkey] || {};
-                map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-                mapkey = curr.replace(/.*packages\/.*?\/src\/main\/(.*?)\/[^/].js$/, '$1')
-                map[mapkey] = map[mapkey] || {};
-                map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-                if (!paths[key2])
-                    paths[key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
-                else {
-                    //todo delete key2 from global paths and blacklist it
+            if (/packages/.test(curr)) {
+                curr = curr.replace(/.*packages/, 'packages')
+                var key1 = curr.replace(/.*packages\/(.*)src\/main\/([^.]*)\.js$/, '$1$2')
+                var key2 = curr.replace(/.*\/([^/.]*)\.js$/, '$1');
+                if (key1 && key1 !== curr) {
+                    paths[key1] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                }
+                if (key2 && key2 !== curr) {
+                    var mapkey = curr.replace(/.*(packages\/.*?)\/src\/main\/.*/, '$1')
+                    map[mapkey] = map[mapkey] || {};
+                    map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                    mapkey = curr.replace(/.*packages\/(.*?)\/src\/main\/.*/, '$1')
+                    map[mapkey] = map[mapkey] || {};
+                    map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                    mapkey = curr.replace(/.*packages\/.*?\/src\/main\/([^/]).*/, '$1')
+                    map[mapkey] = map[mapkey] || {};
+                    map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                    mapkey = curr.replace(/.*packages\/.*?\/src\/main\/(.*?)\/[^/].js$/, '$1')
+                    map[mapkey] = map[mapkey] || {};
+                    map[mapkey][key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                    if (!paths[key2])
+                        paths[key2] = curr.replace(__dirname + '/', '').replace(/\.js$/, '');
+                    else {
+                        //todo delete key2 from global paths and blacklist it
+                    }
                 }
             }
         })
